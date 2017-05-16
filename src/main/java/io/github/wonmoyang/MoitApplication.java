b22352e5184b6ac503773ba88459eb8f7694802a
wmyang <yangwm89@gmail.com>
2017-05-16 15:46:13 +0900

    first init

---
+++ src/main/java/io/github/wonmoyang/MoitApplication.java
@@ -0,0 +1,12 @@
+package io.github.wonmoyang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoitApplication.class, args);
+	}
+}
