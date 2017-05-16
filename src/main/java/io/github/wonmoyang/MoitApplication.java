3d7b9281ec988976bc825f95b55d2ebb823d2685
wmyang <yangwm89@gmail.com>
2017-05-16 16:19:14 +0900

    add configs

--- src/main/java/io/github/wonmoyang/MoitApplication.java
+++ src/main/java/io/github/wonmoyang/MoitApplication.java
@@ -1,12 +1,42 @@
 package io.github.wonmoyang;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import io.github.wonmoyang.config.DefaultProfileUtil;
 
 @SpringBootApplication
 public class MoitApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MoitApplication.class, args);
+	
+	private static final Logger log = LoggerFactory.getLogger(MoitApplication.class);
+	
+	public static void main(String[] args) throws UnknownHostException {
+		SpringApplication app = new SpringApplication(MoitApplication.class);
+		DefaultProfileUtil.addDefaultProfile(app);
+		
+		Environment env = app.run(args).getEnvironment();
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}\n\t" +
+                "External: \t{}://{}:{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol,
+            env.getProperty("server.port"),
+            protocol,
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"),
+            env.getActiveProfiles());
+        
 	}
 }
---
+++ src/main/java/io/github/wonmoyang/config/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package io.github.wonmoyang.config;
+
+public class ApplicationConstants {
+	
+	public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+	public static final String SPRING_PROFILE_PRODUCTION  = "prod";
+
+}
